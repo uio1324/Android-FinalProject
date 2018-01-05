923fb4563d118cc2ad25857862f442f8f0c87183
fishfishfishfishfish <619156737@qq.com>
2018-01-05 23:30:48 +0800

    TaskInfo DB

--- app/src/main/java/com/example/great/project/Database/CourseDB.java
+++ app/src/main/java/com/example/great/project/Database/CourseDB.java
@@ -6,7 +6,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import com.example.great.project.Models.CourseModel;
+import com.example.great.project.Model.CourseModel;
 import com.example.great.project.R;
 
 import java.util.ArrayList;
